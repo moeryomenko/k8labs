281377a49c3e81e6ab5acc169c5ab857a1d2a76d
Maxim Eryomenko <moeryomenko@gmail.com>
2022-03-22 18:59:46 +0300

    feat(kubeadm): update cluster configuration
    Also add cilium CNI install script
    
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+CILIUM_VERSION=1.11.2
+
+# install
+helm install cilium cilium/cilium --version ${CILIUM_VERSION} \
+   --namespace kube-system \
+   --set k8sServiceHost=192.168.111.20 \
+   --set k8sServicePort=6443 \
+   --set nodeinit.enabled=true \
+   --set kubeProxyReplacement=strict \
+   --set hostServices.enabled=false \
+   --set externalIPs.enabled=true \
+   --set nodePort.enabled=true \
+   --set hostPort.enabled=true \
+   --set bpf.masquerade=false \
+   --set image.pullPolicy=IfNotPresent \
+   --set ipam.mode=kubernetes \
+   --set hubble.enabled=true \
+   --set hubble.relay.enabled=true \
+   --set hubble.ui.enabled=true
